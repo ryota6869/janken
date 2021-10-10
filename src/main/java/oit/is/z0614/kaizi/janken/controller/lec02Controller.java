652c7ba90f2bf5fea2156ebd1568483f8c1c6173
ryota6869 <e1b19056@oit.ac.jp>
2021-10-10 10:39:50 +0900

    名前入力の追加と表示

--- src/main/java/oit/is/z0614/kaizi/janken/controller/lec02Controller.java
+++ src/main/java/oit/is/z0614/kaizi/janken/controller/lec02Controller.java
@@ -1,5 +1,24 @@
 package oit.is.z0614.kaizi.janken.controller;
 
-public class lec02Controller {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+@RequestMapping("/lec02.html")
+public class lec02Controller {
+  @GetMapping
+  public String lec02() {
+    return "lec02.html";
+  }
+
+  @PostMapping
+  public String lec02(@RequestParam String name, ModelMap model) {
+    model.addAttribute("name", name);
+    return "lec02.html";
+  }
 }
--- src/main/java/oit/is/z0614/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z0614/kaizi/janken/model/Janken.java
@@ -1,5 +1,13 @@
 package oit.is.z0614.kaizi.janken.model;
 
 public class Janken {
+  String name;
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
 }
