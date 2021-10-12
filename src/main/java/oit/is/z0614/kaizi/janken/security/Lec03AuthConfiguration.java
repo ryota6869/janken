004d4c6ff6f1ed34d434487081af6eed1fa3c54a
ryota6869 <e1b19056@oit.ac.jp>
2021-10-12 21:37:24 +0900

    認証機能の遷移先の修正

--- src/main/java/oit/is/z0614/kaizi/janken/security/Lec03AuthConfiguration.java
+++ src/main/java/oit/is/z0614/kaizi/janken/security/Lec03AuthConfiguration.java
@@ -46,7 +46,5 @@ public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter{
     // antMatchers()の他にanyRequest()と書くとあらゆるアクセス先を表現できる
     // authenticated()の代わりにpermitAll()と書くと認証処理が不要であることを示す
     http.authorizeRequests().antMatchers("/lec02/**").authenticated();
-    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8000/ に戻る
-    http.logout().logoutSuccessUrl("/");
   }
 }
