ed5de7d0870ec48ef4395e00e34c52efdd1e3756
ryota6869 <e1b19056@oit.ac.jp>
2021-10-19 20:55:14 +0900

    データベースの追加

---
+++ src/main/java/oit/is/z0614/kaizi/janken/model/User.java
@@ -0,0 +1,14 @@
+package oit.is.z0614.kaizi.janken.model;
+
+public class User {
+  String user;
+
+  public String getUser() {
+    return user;
+  }
+
+  public void setUser(String user) {
+    this.user = user;
+  }
+
+}
