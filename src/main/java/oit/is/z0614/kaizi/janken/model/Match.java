9b3566ffd4975337623d480f32aac2ff87800c2d
ryota6869 <e1b19056@oit.ac.jp>
2021-10-22 11:39:11 +0900

    DBの試合情報を表示

---
+++ src/main/java/oit/is/z0614/kaizi/janken/model/Match.java
@@ -0,0 +1,42 @@
+package oit.is.z0614.kaizi.janken.model;
+
+public class Match {
+  int id;
+  String user1Hand;
+  int user1;
+  String user2Hand;
+  int user2;
+
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getUser1Hand() {
+    return user1Hand;
+  }
+  public void setUser1Hand(String user1Hand) {
+    this.user1Hand = user1Hand;
+  }
+  public int getUser1() {
+    return user1;
+  }
+  public void setUser1(int user1) {
+    this.user1 = user1;
+  }
+  public String getUser2Hand() {
+    return user2Hand;
+  }
+  public void setUser2Hand(String user2Hand) {
+    this.user2Hand = user2Hand;
+  }
+  public int getUser2() {
+    return user2;
+  }
+  public void setUser2(int user2) {
+    this.user2 = user2;
+  }
+
+
+}
